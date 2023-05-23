8051f8b8c5736fdb88d3ebc00a7fd798343054f9
Tetsuro Kai <tetsuro.kai87@gmail.com>
2023-05-23 16:53:19 -0700

    main section implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Header from './components/header/header';
+import Hero from './components/hero/hero';
+import MainSection from './components/main-section/main-section';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      TK
+      <Header/>
+      <Hero/>
+      <MainSection/>
     </div>
   );
 }
